362c3b05e9296c1f2aef33f5985e5aaff6c797b3
Shiv03072002 <ushiv.kumar@7lives.agency>
2026-03-12 12:58:35 +0530

    nav padding issue resolved

--- components/common/Navbar.jsx
+++ components/common/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
     return (
         <>
             <div className="bg-white sticky top-0 z-50 transition-all duration-300" >
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-2">
                     <div className="flex items-center justify-between h-18 md:h-20">
                         
                         {/* Logo - Links to home */}
